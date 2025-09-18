e7e55454421e26cf1bab8c437269dc14e125f060
Kavindu Rajapaksha <kavindurajapaksha848@gmail.com>
2025-09-18 11:15:07 +0530

    Create the arrays and get count of records

--- HostelMate.java
+++ HostelMate.java
@@ -1,4 +1,27 @@
+import java.util.Scanner;
+
+//Main Method of the application
 public class HostelMate {
+
+    // Data Tables for array
+
+    // 100 - maximum number of rooms and 6-attributes for each room
+    static String[][] rooms = new String[100][5];
+
+    // 100 - maximum number of students and 6-attributes for each student
+    static String[][] students = new String[100][5];
+
+    // 100 - maximum number of allocationns and 6-attributes for each allocatiions
+    static String[][] allocations = new String[100][5];
+
+    // Stored the occupancy
+    static String[][] occupancy = new String[100][10];
+
+    // get the count of records
+    static int countofstudents = 0; // count of students
+    static int countofrooms = 0; // count of rooms
+    static int countofallocations = 0; // count of allocations
+
     public static void main(String[] args) {
 
     }
